10abb3a7f8c8b93993eee86992762153f8447d39
dulmi-k <dulmikr@gmail.com>
2022-10-22 18:18:28 +0100

    change SASS to SCSS and added particles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portfolio';
+
+  private overlay: HTMLElement;
+
+  constructor(
+    private overlayContainer: OverlayContainer,
+    public theme: ThemeService
+  ) {
+    // pass the container element to theme service
+    this.overlay = this.overlayContainer.getContainerElement();
+    this.theme.init(this.overlay);
+  }
+
+  @HostListener('window:resize')
+  OnWindowResize() {
+    this.theme.applyParticles(this.theme.configLocation);
+  }
 }
